f5dbefc55576f06deea4a575a6774751cab9d7bf
Taylor Jensen <86851462+Taylorlcj@users.noreply.github.com>
2022-09-07 15:29:58 -0400

    Add files via upload

---
+++ LabOne/src/LabOne.java
@@ -0,0 +1,17 @@
+/*
+ * LabOne.java
+ * Written By: Taylor Jensen
+ * CS 225 Fall 2022
+ */
+public class LabOne {
+    public static int year;
+    public static void main(String[] args) {
+        //class variables
+        String firstName="Taylor";
+        String lastName="Jensen";
+        year=2022;
+        System.out.println("Hello My Name is: ");
+        System.out.println(firstName +" " + lastName);
+        System.out.println("This is my first program of Fall " + year);
+    }
+}
