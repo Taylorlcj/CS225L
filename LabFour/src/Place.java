e3ad0350ec5e751d0801f04c526ab157d0288d33
Taylor Jensen <Taylorlcj@gmail.com>
2022-10-05 15:13:25 -0400

    Lab 4 10/5/22

---
+++ LabFour/src/Place.java
@@ -0,0 +1,61 @@
+
+public class Place {
+	
+	public int getPlaceNumber() {
+		return placeNumber;
+	}
+
+
+	public void setPlaceNumber(int placeNumber) {
+		this.placeNumber = placeNumber;
+	}
+
+
+	public int getJumpTo() {
+		return jumpTo;
+	}
+
+
+	public void setJumpTo(int jumpTo) {
+		this.jumpTo = jumpTo;
+	}
+
+
+	public boolean isSnake() {
+		return isSnake;
+	}
+
+
+	public void setSnake(boolean isSnake) {
+		this.isSnake = isSnake;
+	}
+
+
+	public boolean isLadder() {
+		return isLadder;
+	}
+
+
+	public void setLadder(boolean isLadder) {
+		this.isLadder = isLadder;
+	}
+
+
+	private int placeNumber;
+	private int jumpTo;
+	private boolean isSnake;
+	private boolean isLadder;
+	
+	
+	public Place(int placeNumber, int jumpTo, boolean isSnake, boolean isLadder) {
+		
+		this.placeNumber = placeNumber;
+		this.jumpTo = jumpTo;
+		this.isSnake = isSnake;
+		this.isLadder = isLadder;
+		
+	}
+	
+	
+
+}
---
+++ LabFour/src/SnakesAndLaddersController.java
@@ -0,0 +1,108 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+public class SnakesAndLaddersController {
+	
+	private Place[] board;
+	private File boardFile;
+	private File newBoardFile;
+	
+	public static void main(String[] args) {
+		SnakesAndLaddersController slc = new SnakesAndLaddersController();
+		
+
+		
+		//Running the Program
+		slc.readBoardFromFile();
+		slc.printBoard();
+		slc.changeBoardValues();
+		slc.printBoard();
+		slc.saveBoard();
+		
+	}
+	
+	public SnakesAndLaddersController() {
+		
+		boardFile = new File("SnakesAndLaddersBoard.csv");
+		newBoardFile = new File("UpdatedSnakesAndLaddersBoard.csv");
+		board = new Place[100];
+		
+	}
+	
+	public void readBoardFromFile() {
+		//read the file and create Place objects based on the values in board
+		//Example of how to create a place object
+		//!! IMPORTANT the order the variables written in SnakesAndLadders.csv are the same as the input for the Place object
+		//									place #, jumpTo, isSnake, isLadder
+		Place examplePlaceObject = new Place(1,30,false,true);
+		//Then if you wanted to add that place object to the first spot in the array you could
+		
+		board[0] = examplePlaceObject;
+		
+		try {
+			FileReader fr = new FileReader(boardFile);
+			BufferedReader br = new BufferedReader(fr);
+			// make a String called line
+			String line;
+
+			// while line is equal to the next line of the bufferedreader is not equal to null
+			// this means read the next line in the file until there are not more line to read
+			while (  ( line = br.readLine() ) != null     ) {
+
+				// make an array to hold the columns 
+				String[] lineColumns;
+				// break the line up to columns. break on the comma and delete the comma
+				lineColumns = line.split(",");
+ 
+				// print every line
+				System.out.println(line);
+
+
+				//Change these values to the values found in the file
+				int placeNumber = 0;
+				int jumpTo = 0;
+				boolean isSnake = false;
+				boolean isLadder = false; 
+
+				board[i] = new Place(placeNumber, jumpTo, isSnake, isLadder);
+				i++;
+				
+				//LineColumns is now an array of strings that holds the values in the .csv file
+
+
+
+			}
+			
+			// when completely done with reading close the Reader
+			br.close();
+
+		} catch (Exception e) {
+
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void printBoard() {
+		
+		for(int i = 0; i < board.length; i++) {
+			System.out.println("Place Number: " + board[i].getPlaceNumber() + " Jump to: " + board[i].getJumpTo() + " Is Snake: " + board[i].isSnake() + " Is Ladder " + board[i].isLadder());
+		}
+		
+	}
+	
+	public void changeBoardValues() {
+		
+		
+		
+	}
+	
+	public void saveBoard() {
+		
+		
+		
+	}
+	
+
+}
