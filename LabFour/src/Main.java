0919ea19ab6eed733a3ddfca41ffb9d7d3b0a4a1
Taylor Jensen <Taylorlcj@gmail.com>
2022-10-08 20:51:10 -0400

    Lab 4 10/8/22

--- LabFour/src/Main.java
+++ LabFour/src/Main.java
@@ -1,3 +1,11 @@
+/*
+*******************************************************
+ Class: SnakesAndLaddersController
+ Author: Taylor Jensen
+ Date Created: Oct 8, 2022
+ Attributes: None
+********************************************************
+*/
 public class Main {
     public static void main(String[] args) {
 
