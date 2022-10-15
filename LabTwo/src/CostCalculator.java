78353217832678735500b5f3f2eef1dbf1b7eea2
Taylor Jensen <Taylorlcj@gmail.com>
2022-10-14 21:53:43 -0400

    Lab 2 10/14/22

--- LabTwo/src/CostCalculator.java
+++ LabTwo/src/CostCalculator.java
@@ -9,14 +9,38 @@
 // ********************************************** 
 public class CostCalculator {
 	
-	public static void main(String args) {
+	public static void main(String[] args) {
 		//Parameters for truck 1
 		int truck1Capacity = 30;
 		int truck1FuelBurn = 50;
 		int truck1Cost = 10000;
+		int truck1total = totalCost(truck1Capacity,truck1Cost,truck1FuelBurn);
+		System.out.println("Truck 1 total cost: $" + truck1total);
+
+		//Parameters for truck 1
+		int truck2Capacity = 40;
+		int truck2FuelBurn = 70;
+		int truck2Cost = 13000;
+		int truck2total = totalCost(truck2Capacity,truck2Cost,truck2FuelBurn);
+		System.out.println("Truck 2 total cost: $" + truck2total);
+
+		//Parameters for truck 1
+		int truck3Capacity = 80;
+		int truck3FuelBurn = 100;
+		int truck3Cost = 20000;
+		int truck3total = totalCost(truck3Capacity,truck3Cost,truck3FuelBurn);
+		System.out.println("Truck 3 total cost: $" + truck3total);
 		
 		//using if else and the totalCosts function find and return the best truck
-		
+		if (truck1total < truck2total && truck1total <truck3total){
+			System.out.println("Truck 1 is the best!");
+		}
+		else if (truck2total < truck1total && truck2total <truck3total){
+			System.out.println("Truck 2 is the best!");
+		}
+		else{
+			System.out.println("Truck 3 is the best!");
+		}
 	}
 	
 	public static int totalCost(int capacity, int fuelBurn, int vehicleCost) {
@@ -26,12 +50,12 @@ public class CostCalculator {
 	
 	public static int fuelCosts(int fuelBurn, int numTrips) {
 		//calculate fuel costs here 
-		return 0;
+		return fuelBurn  * numTrips  * 5;
 	}
 	
 	public static int numTrips(int capacity) {
 		//calculate number of trips here
-		return 0;
+		return 120 / capacity;
 	}
 
 }
